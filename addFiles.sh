145db03efa532fd31fc5a9fa98ea204e038ce50e
lilith <pantonk267@gmail.com>
2023-11-30 20:29:49 -0500

    add fire/waterfox chrome folder

--- addFiles.sh
+++ addFiles.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 cd "${0%/*}"
 
+CHROME_DIR=$({
+        find ~/.mozilla/firefox -type d -name "*.default-release"
+} || {
+        find ~/.waterfox -type d -name "*.default-release"
+})
+
 cp -r .config/* ~/.config
 cp -r .themes/* ~/.themes
 cp -r flavours/* ~/.local/share/flavours/
+cp -r chrome/* $CHROME_DIR/chrome/
