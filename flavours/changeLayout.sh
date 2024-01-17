3c2d8f2a81f4f5dd3584e5dbb8264952e508aedc
lilith <pantonk267@gmail.com>
2024-01-17 12:15:28 -0500

    minor changes

--- flavours/changeLayout.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "${0%/*}"
-
-ls ./base16/templates/flavours/templates
-read -p "Enter Style: " style
-cp ./base16/templates/flavours/templates/$style.mustache ~/.config/flavours/config.toml
